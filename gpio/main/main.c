598292f303d1ba4508fdcc7b81adda91b99e2a4a
root <d.rahul.dd@gmail.com>
2022-01-23 09:55:56 +0530

    program to blink LED in esp32

---
+++ gpio/main/main.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<freertos/FreeRTOS.h>
+#include<freertos/task.h>
+#include<driver/gpio.h>
+
+BaseType_t result;
+
+void tasklog(void *pv)
+{
+	while(1)
+	{
+		printf("Sensor Task\n");
+		vTaskDelay(1000/portTICK_PERIOD_MS);
+	}
+}
+
+void blink(void *pv)
+{
+	esp_rom_gpio_pad_select_gpio(2);
+	gpio_set_direction(2,GPIO_MODE_OUTPUT);
+
+	while(1)
+	{
+		gpio_set_level(2,1);
+		vTaskDelay(1500/portTICK_PERIOD_MS);
+//		printf("Set\n");
+		gpio_set_level(2,0);
+		vTaskDelay(1500/portTICK_PERIOD_MS);
+//		printf("Reset\n");
+	}
+}
+
+int app_main()
+{
+	result=xTaskCreate(tasklog,"Tasklog",2048,NULL,4,NULL);
+	if(result!=pdPASS)
+	{
+		printf("Task Log Creation Failed\n");
+		return 0;
+	}
+
+	result=xTaskCreate(blink,"Blink",2048,NULL,4,NULL);
+	if(result!=pdPASS)
+	{
+		printf("Task Blink Creation Failed\n");
+		return 0;
+	}
+	
+	return 0;
+}
