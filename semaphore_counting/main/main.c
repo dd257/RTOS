db4c7b014fa8f5220aff7f64f40dfd67a2141ad0
root <d.rahul.dd@gmail.com>
2022-01-25 12:01:46 +0530

    Question 21

---
+++ semaphore_counting/main/main.c
@@ -0,0 +1,61 @@
+//	Question 21
+
+#include<stdio.h>
+#include<freertos/FreeRTOS.h>
+#include<freertos/task.h>
+#include<freertos/semphr.h>
+
+BaseType_t result;
+SemaphoreHandle_t sem;
+int count=0;
+
+void receive(void *pv)
+{
+	while(1)
+	{
+		
+		xSemaphoreTake(sem,portMAX_DELAY);
+		xSemaphoreTake(sem,portMAX_DELAY);
+		xSemaphoreTake(sem,portMAX_DELAY);
+		xSemaphoreTake(sem,portMAX_DELAY);
+		xSemaphoreTake(sem,portMAX_DELAY);
+		count=uxSemaphoreGetCount(sem);
+		printf("Received Count: %d\n",count);
+		vTaskDelay(1000/portTICK_PERIOD_MS);
+	}
+}
+
+void send(void *pv)
+{
+	while(1)
+	{
+		xSemaphoreGive(sem);
+		count=uxSemaphoreGetCount(sem);
+		printf("Sent Count: %d\n",count);
+		vTaskDelay(1000/portTICK_PERIOD_MS);
+	}
+}
+
+int app_main()
+{
+	sem=xSemaphoreCreateCounting(3,3);
+
+	if(sem==NULL)
+		printf("Semaphore not created\n");
+	
+	result=xTaskCreate(receive,"Receive",2048,NULL,5,NULL);
+	if(result!=pdPASS)
+	{
+		printf("Task Receive Creation Failed\n");
+		return 0;
+	}
+	
+	result=xTaskCreate(send,"Send",2048,NULL,4,NULL);
+	if(result!=pdPASS)
+	{
+		printf("Task Send Creation Failed\n");
+		return 0;
+	}
+	
+	return 0;
+}
