aab1d493c62e2ef38a1b4b8bb5853b71df0b4079
root <d.rahul.dd@gmail.com>
2022-01-26 12:54:17 +0530

    Question 12

--- en_dis_interrupt/main/main.c
+++ en_dis_interrupt/main/main.c
@@ -1,3 +1,5 @@
+//Question_12
+
 #include <stdio.h>
 #include <driver/gpio.h>
 #include <freertos/FreeRTOS.h>
@@ -11,64 +13,77 @@ intr_handler_t isr_handler;
 
 void task(void *pv)
 {
+	bool gpio_state;
+
  	while(1)
 	{  
 		gpio_set_level(13,0);
-		printf("Main Task\nPin State: %d\n",gpio_get_level(12));
+		gpio_state=gpio_get_level(14);
+		printf("Main Task\nPin State: %d\n",gpio_state);
 		vTaskDelay(1000/portTICK_PERIOD_MS);
  	}
 }
 
 void int_task(void *pv)
 {
- 	//while(1)
-	// {  
-		vTaskSuspend(isr_task);
-		gpio_set_level(13,1);	
-		vTaskDelay(1000/portTICK_PERIOD_MS);
- 	//}
+	vTaskSuspend(NULL);
+	gpio_set_level(13,1);	
+	vTaskDelay(1000/portTICK_PERIOD_MS);
 }
 
 void IRAM_ATTR intr(void *pv)
-//void intr(void *pv)
 {
  	xTaskResumeFromISR(isr_task);
 }
 
+
 int app_main()
 {
-   
-    gpio_reset_pin(12);
+	BaseType_t result;
+
+    gpio_reset_pin(14);
 	gpio_reset_pin(13);
-  	gpio_set_direction(12, GPIO_MODE_INPUT);
+  	gpio_set_direction(14, GPIO_MODE_INPUT);
 	gpio_set_direction(13, GPIO_MODE_OUTPUT);
-	gpio_intr_enable(12);
+	gpio_intr_enable(14);
 	
-  	gpio_set_intr_type(12, GPIO_INTR_LOW_LEVEL);
+  	gpio_set_intr_type(14, GPIO_INTR_LOW_LEVEL);
 	if(intr_result!=ESP_OK)
 	{
 		perror("Interrupt type");
 		return 0;
 	}  
 
-	gpio_install_isr_service(ESP_INTR_FLAG_IRAM);
+	gpio_install_isr_service(ESP_INTR_FLAG_EDGE); //ESP_INTR_FLAG_EDGE
 	if(intr_result!=ESP_OK)
 	{
 		perror("Interrupt service");
 		return 0;
 	}
 
-  	gpio_isr_handler_add(12, int_task, NULL);
+  	gpio_isr_handler_add(14, int_task, NULL);
 	if(intr_result!=ESP_OK)
 	{
 		perror("Interrupt not enabled");
 		return 0;
 	}
 
-	xTaskCreate(task, "task", 4096, NULL , 10, &main_task);
-	xTaskCreate(int_task, "int_task", 4096, NULL , 10, &isr_task);
-	vTaskStartScheduler();
+	result=xTaskCreate(task, "task", 4096, NULL , 5, &main_task);
+	if(result!=pdTRUE)
+	{
+		perror("Main Task not created");
+		return 0;
+	}
+	
+	result=xTaskCreate(int_task, "interrupt_task", 4096, NULL , 6, &isr_task);
+	if(result!=pdTRUE)
+	{
+		perror("Interrupt Task not created");
+		return 0;
+	}
 
+	vTaskStartScheduler();
+//	vTaskEndScheduler();
 	return 0;
 }
 
